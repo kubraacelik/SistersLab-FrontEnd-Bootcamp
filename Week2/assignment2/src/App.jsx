bd9253424b2e327324094979c9e33202f392891b
kubraacelik <kkbra.celik92@gmail.com>
2023-10-21 18:22:45 +0300

    first commit

--- Week2/assignment2/src/App.jsx
+++ Week2/assignment2/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DataTable from "./components/DataTable";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  var cars = [
+    {
+      id: 1,
+      brand: "Mercedes-Benz",
+      model: "E-Serisi"
+    },
+    {
+      id: 2,
+      brand: "Volkswagen",
+      model: "Golf"
+    },
+    {
+      id: 3,
+      brand: "BMW",
+      model: "3 Serisi"
+    },
+    {
+      id: 4,
+      brand: "Ford",
+      model: "Mustang"
+    }
+  ];
+
+  const title = "Car List";
+  const text = "Our Cars";
+  const number = 4;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <DataTable title={title} data={cars} text={text} number={number}></DataTable>
+    </div>
+  );
 }
 
-export default App
+export default App;
