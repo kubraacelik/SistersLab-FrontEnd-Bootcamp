c3b9815fd61de0b17e8ba5df03330d8aae2342ab
kubraacelik <kkbra.celik92@gmail.com>
2023-11-02 22:51:56 +0300

    added hamburgerapp

---
+++ Week3/hamburgerapp/src/toAdd.jsx
@@ -0,0 +1,35 @@
+const toAdd = [
+    {
+      id: 1,
+      name: 'Marul',
+      color: 'green',
+      price: 8,
+      count: 0,
+    },
+  
+    {
+      id: 2,
+      name: 'Peynir',
+      color: '#ffa600',
+      price: 15,
+      count: 0,
+    },
+  
+    {
+      id: 3,
+      name: 'Köfte',
+      color: 'brown',
+      price: 26,
+      count: 0,
+    },
+  
+    {
+      id: 4,
+      name: 'Domates',
+      color: 'red',
+      price: 16,
+      count: 0,
+    },
+  ];
+  
+  export default toAdd;
